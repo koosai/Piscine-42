c939369bd2771466dfa8ce436976f18e91fb68e3
koosai <pinassow@gmail.com>
2022-10-31 00:41:16 +0300

    Update ft_find_next_prime.c

--- C/c05/ex07/ft_find_next_prime.c
+++ C/c05/ex07/ft_find_next_prime.c
@@ -1,4 +1,46 @@
 #include <unistd.h>
 
+int ft_sqrt(int nb)
+{
+  int root;
+  
+  root = 0;
+  while (root * root < nb)
+    root++;
+  if (root * root == nb)
+    return (root);
+  else
+    return (0);
+}
+
+int ft_is_prime(int nb)
+{
+  int i;
+  int sq;
+  
+  i = 2;
+  sq = ft_sqrt(nb);
+  if （nb < 2)
+    return (0);
+  while (i < sq)
+  {
+    if (nb % i == 0)
+      return (0);
+    i++;
+  }
+  return (1);
+}
+
 int ft_find_next_prime(int nb)
 {
+  nb++;
+  while (!ft_is_prime(nb))
+    nb++;
+  return (nb);
+}
+
+int 	main(void)
+{
+printf("%d",ft_find_next_prime(4));	
+	return (0);
+}
